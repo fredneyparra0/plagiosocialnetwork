bc26b4f1b734d91ccbc0440f1e74f300654f4638
ney <fredneyparra0@gmail.com>
2021-05-17 19:44:20 -0400

    bd creada para contactos y html

--- router/router.js
+++ router/router.js
@@ -7,12 +7,29 @@ rootRouter.get('/', (req, res) => {
 
 rootRouter.use('/auth', require('../router/auth.router'));
 
+
+const modelContact = require('../models/personcontact')
+rootRouter.post('/authnew', async (req, res) => {
+    const body = req.body;
+    const person = new modelContact(body);
+    await person.save();
+});
+
 rootRouter.get('/home-user', async (req, res) => {
+    const userLogged = req.session.userLogged;
+    
+    if (!userLogged) return res.redirect('/auth/login')
+    
+    return res.render('home', {user: userLogged, contacts: contacts});
+});
+
+rootRouter.get('/home-user/contacts', async (req, res) => {
     const userLogged = req.session.userLogged;
 
     if (!userLogged) return res.redirect('/auth/login')
-
-    return res.render('home', {user: userLogged});
-});
+    
+    const contacts = await modelContact.find();
+    res.render('contacts', {user: userLogged, contacts: contacts})
+})
 
 module.exports = rootRouter;
